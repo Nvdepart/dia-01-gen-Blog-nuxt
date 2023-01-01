8f06a2a3d917d1e1cd78c5f183370c9a0ce3343a
Nvdepart <67013480+Nvdepart@users.noreply.github.com>
2023-01-01 01:12:54 +0100

    neu project

---
+++ codi/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+   
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer>
+    <span align:center>Atlas-Expo</span>
+    <br>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  //
+}
+</script>
